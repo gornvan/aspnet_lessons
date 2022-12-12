a23c427646b06d6fdb699ff3049537e252511147
gornvan <name@example.com>
2022-12-12 20:38:11 +0100

    add Error Pages as string constants and templates

---
+++ lesson11_logging_and_error_middleware/MVC/ErrorHandling/ErrorPages.cs
@@ -0,0 +1,31 @@
+﻿namespace lesson11_serilog.ErrorHandling
+{
+    public static class ErrorPages
+    {
+        public const string InternalErrorPage = @"
+                <html> <body> 
+                Internal error happened. Please go to the previous page.
+                <html/> <body/>
+            ";
+
+
+        public const string NotFoundPage = @"
+                <html> <body> 
+                <strong>The page You have requested is not available right now. :( <strong/>
+                <html/> <body/>
+            ";
+
+        public const string BadRequestPageTemplate = @"
+                <html> <body> 
+                <strong>Badly formed request:<strong/>
+                {Message}
+                <html/> <body/>
+            ";
+
+        public const string FatalErrorPage = @"
+                <html> <body> 
+                The server is currently unavailable. Please contact the administrator.
+                <html/> <body/>
+            ";
+    }
+}
