e8fa2eb5f37ec60c409830ffb3715ac1281b5a5c
gornvan <name@example.com>
2022-12-12 20:40:31 +0100

    add serilog configurator with hard-coded settings

---
+++ lesson11_logging_and_error_middleware/MVC/Logging/SerilogConfigurator.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace lesson11_serilog.Logging
+{
+    public class SerilogConfigurator
+    {
+        public static void ConfigureSerilog(WebApplicationBuilder builder, LogLevel minimumLogLevel, string filename)
+        {
+            var outputTemplate = 
+                $"{{Timestamp:HH:mm}} [l:{{Level}}] (th:{{ThreadId}}) " +
+                $"Message: {{Message}}{{NewLine}}Esception: {{Exception}}";
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(outputTemplate: outputTemplate)
+                .WriteTo.File(filename, outputTemplate: outputTemplate)
+                .MinimumLevel.Is((LogEventLevel)minimumLogLevel)
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+        }
+    }
+}
