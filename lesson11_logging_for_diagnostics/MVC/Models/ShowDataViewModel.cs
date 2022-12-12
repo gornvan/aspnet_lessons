9968354935725ec119c39cc954eefe75c8d666a0
gornvan <name@example.com>
2022-12-12 20:31:34 +0100

    copy lesson 10 to lesson 11

---
+++ lesson11_logging_for_diagnostics/MVC/Models/ShowDataViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lesson10_validation.Models
+{
+    public class ShowDataViewModel
+    {
+        [Required]
+        public string Text { get; set; }
+
+        public int Year { get; set; }
+
+        [Required]
+        public bool? Approval { get; set; }
+    }
+}
