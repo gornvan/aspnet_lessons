fa39c6667ee24ce37080edd6a68e170207400004
gornvan <name@example.com>
2022-12-22 01:10:04 +0100

    add a startup class to resolve and use the admin creation service

---
+++ lesson14_Authentication/AuthenticatedWebApp/AuthenticatedWebApp/Startup.cs
@@ -0,0 +1,20 @@
+﻿using AuthenticatedWebApp.Data.Seed;
+
+namespace AuthenticatedWebApp
+{
+    public class Startup
+    {
+        /// <summary>
+        /// call this AFTER registering the identity providers AND migrating the DB
+        /// </summary>
+        /// <param name="services"></param>
+        public static async Task InitializeIdentities(IServiceProvider services)
+        {
+            using (var scope = services.CreateScope())
+            {
+                var identitySeeder = scope.ServiceProvider.GetService<CreateDefaultUserService>();
+                await identitySeeder.CreateRoles();
+            }
+        }
+    }
+}
