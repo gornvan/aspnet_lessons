5b4e0c9fbda9a6277e192d030a6fd549bbc213e4
gornvan <name@example.com>
2023-01-04 18:07:11 +0100

    remove failed attempt to create a component outside a module

--- lesson16_angular/lesson/src/_parrot/parrot.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'parrot',
-  templateUrl: './parrot.component.html'
-})
-export class ParrotComponent {
-  title = 'parrot';
-}
--- lesson16_angular/lesson/src/app/app.module.ts
+++ lesson16_angular/lesson/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ParrotComponent } from '../parrot/parrot.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ParrotComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
