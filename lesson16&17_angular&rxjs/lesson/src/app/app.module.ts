9a2a71ffe50d2ce78a8a77518036f97291a3a527
gornvan <name@example.com>
2023-01-09 17:45:04 +0100

    Add an App Module-level dependency on HttpClientModule

--- lesson16&17_angular&rxjs/lesson/src/app/app.module.ts
+++ lesson16&17_angular&rxjs/lesson/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ParrotComponent } from './parrot/parrot.component';
 
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
