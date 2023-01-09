f8dd65aa48c875eb78d80d30cd7b03957afde6a4
gornvan <name@example.com>
2023-01-09 17:46:32 +0100

    create People Service for pulling the Person[] array

---
+++ lesson16&17_angular&rxjs/lesson/src/app/services/people.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Person } from '../models/person';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeopleService {
+  constructor(private httpClient: HttpClient) { }
+  getPeople() : Observable<Person[]> {
+    return this.httpClient.get<Person[]>('assets/people.json');
+  }
+}
