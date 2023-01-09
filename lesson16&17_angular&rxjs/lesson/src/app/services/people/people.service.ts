3a49e6b918bf6794ee9f0e15c5818080d64eb35e
gornvan <name@example.com>
2023-01-09 17:58:16 +0100

    People service: add a method for getting an array of names

--- lesson16&17_angular&rxjs/lesson/src/app/services/people/people.service.ts
+++ lesson16&17_angular&rxjs/lesson/src/app/services/people/people.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Person } from '../../models/person';
-import { Observable, throwError } from 'rxjs';
+import { map, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
   constructor(private httpClient: HttpClient) { }
+
   getPeople() : Observable<Person[]> {
     return this.httpClient.get<Person[]>('assets/people.json');
   }
+
+  getPeopleNames(): Observable<string[]>
+  {
+    return this.getPeople().pipe(map(
+      people =>{
+        return people.map(p => p.Name);
+      }
+    ));
+  }
+
 }
