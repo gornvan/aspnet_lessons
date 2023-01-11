01646d2cc6ca7d4bac3a8f49cc527bf1385670a4
gornvan <name@example.com>
2023-01-11 19:42:58 +0100

    appcomponent: depend on cat food service to get the food and display whether the cat is fed app module: register the cat food service

--- lesson18_angularTests/catsTesting/src/app/app.component.ts
+++ lesson18_angularTests/catsTesting/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { CatfoodService } from './services/catfood.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    CatfoodService
+  ],
 })
 export class AppComponent {
+  constructor(private catFoodService: CatfoodService){}
+
   title = 'catsTesting';
+  food : string = "";
+
+  ngOnInit() {
+    this.food = this.catFoodService.requestFood();
+  }
 }
--- lesson18_angularTests/catsTesting/src/app/app.module.ts
+++ lesson18_angularTests/catsTesting/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CatfoodService } from './services/catfood.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CatfoodService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
