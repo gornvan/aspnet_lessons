d61cf2f951f60b1408aa209884b652cb006f4835
gornvan <name@example.com>
2023-01-11 20:08:32 +0100

    app component spec: format document and add a couple semicolons

--- lesson18_angularTests/catsTesting/src/app/app.component.spec.ts
+++ lesson18_angularTests/catsTesting/src/app/app.component.spec.ts
@@ -5,25 +5,25 @@ import { AppComponent } from './app.component';
 import { CatfoodService } from './services/catfood.service';
 
 describe('AppComponent', () => {
-  let appComponent : AppComponent;
-  let appCompFixture : ComponentFixture<AppComponent>
+  let appComponent: AppComponent;
+  let appCompFixture: ComponentFixture<AppComponent>;
   let catFoodServiceStub: StubCatfoodService;
 
-  function initStubs(){
+  function initStubs() {
     catFoodServiceStub = new StubCatfoodService();
   }
-  async function initComponent(){
-    await TestBed 
-    .configureTestingModule({ 
-      declarations: [
-        AppComponent
-      ],
-      providers: [
-        { provide: CatfoodService, useValue: {}}
-      ]
-    })
-    .overrideProvider(CatfoodService, {useValue: catFoodServiceStub})
-    .compileComponents();
+  async function initComponent() {
+    await TestBed
+      .configureTestingModule({
+        declarations: [
+          AppComponent
+        ],
+        providers: [
+          { provide: CatfoodService, useValue: {} }
+        ]
+      })
+      .overrideProvider(CatfoodService, { useValue: catFoodServiceStub })
+      .compileComponents();
 
     appCompFixture = TestBed.createComponent(AppComponent);
     appComponent = appCompFixture.componentInstance;
@@ -38,12 +38,12 @@ describe('AppComponent', () => {
     expect(appComponent).toBeTruthy();
   });
 
-  it(`should have as title 'catsTesting'`,  async () => {
+  it(`should have as title 'catsTesting'`, async () => {
     await initComponent();
     expect(appComponent.title).toEqual('catsTesting');
   });
 
-  it('should render title',  async () => {
+  it('should render title', async () => {
     await initComponent();
     appCompFixture.detectChanges();
     const compHtmlElement = appCompFixture.nativeElement as HTMLElement;
@@ -51,7 +51,7 @@ describe('AppComponent', () => {
       .toContain('catsTesting app is running!');
   });
 
-  it('shows that cat is hungry in case there is no food',  async ()=> {
+  it('shows that cat is hungry in case there is no food', async () => {
     catFoodServiceStub.setFood('');
     expect(catFoodServiceStub.requestFoodCalled).toEqual(0);
     await initComponent();
@@ -59,12 +59,12 @@ describe('AppComponent', () => {
     appCompFixture.detectChanges();
 
     const text = appCompFixture.nativeElement.querySelector('#catStat').textContent;
-    expect(text).toContain('hungry!')
+    expect(text).toContain('hungry!');
   });
 
   it('tries to feed the cat', async () => {
     await initComponent();
     appCompFixture.detectChanges();
-    expect(catFoodServiceStub.requestFoodCalled).toEqual(1); 
+    expect(catFoodServiceStub.requestFoodCalled).toEqual(1);
   });
 });
