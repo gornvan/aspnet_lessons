bc552c5bbdea0160a3e13f2c3a17d49234696b1f
gornvan <name@example.com>
2023-01-11 19:40:16 +0100

    remove cat component

--- lesson18_angularTests/catsTesting/src/app/app.module.ts
+++ lesson18_angularTests/catsTesting/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CatComponent } from './cat/cat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CatComponent
   ],
   imports: [
     BrowserModule
