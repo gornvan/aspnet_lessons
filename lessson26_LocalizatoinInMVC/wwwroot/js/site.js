44c653964e477a8c1e01e5a892a632cb0cecf9be
gornvan <name@example.com>
2023-02-20 19:24:01 +0100

    add a way of setting a Culture cookie through new CultureSelectorController, via ajax GET and via Redirect following - as two sets of buttons on the header of the Layout

--- lessson26_LocalizatoinInMVC/wwwroot/js/site.js
+++ lessson26_LocalizatoinInMVC/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('.ajax-link').click(async e => {
+    e.preventDefault();
+
+    let element = e.target;
+    let link = element.getAttribute('href');
+
+    await $.ajax(link);
+})
