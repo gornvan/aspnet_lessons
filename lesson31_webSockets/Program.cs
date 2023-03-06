5b905a1cb27b89acc3cc6cfe99135998bc444979
gornvan <name@example.com>
2023-03-06 18:13:20 +0100

    raw WebSocket communication(echoing) demo

--- lesson31_webSockets/Program.cs
+++ lesson31_webSockets/Program.cs
@@ -1,3 +1,5 @@
+using lesson31_webSockets;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+
+app.UseWebSockets(webSocketOptions);
+WebSocketPathBinder.BindWebSocketPath(app, "/ws");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
---
+++ lesson31_webSockets/WebSocketPathBinder.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.WebSockets;
+
+namespace lesson31_webSockets
+{
+    public static class WebSocketPathBinder
+    {
+        private static async Task EchoUntilEnd(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            var receiveResult = await webSocket.ReceiveAsync(
+                new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            while (!receiveResult.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(
+                    new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+                    receiveResult.MessageType,
+                    receiveResult.EndOfMessage,
+                    CancellationToken.None);
+
+                receiveResult = await webSocket.ReceiveAsync(
+                    new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+
+            await webSocket.CloseAsync(
+                receiveResult.CloseStatus.Value,
+                receiveResult.CloseStatusDescription,
+                CancellationToken.None);
+        }
+
+
+        public static void BindWebSocketPath(WebApplication app, string pathPrefix)
+        {
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.StartsWithSegments(pathPrefix))
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await EchoUntilEnd(webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    }
+                }
+                else
+                {
+                    await next(context);
+                }
+            });
+        }
+    }
+}
