a1daa83530bd5c41810c2533ae259ea2add4e6ab
gornvan <name@example.com>
2023-03-06 17:36:15 +0100

    lesson 31: createa empty mvc to start demonstrating websockets

---
+++ lesson31_webSockets/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using lesson31_webSockets.Models;
+
+namespace lesson31_webSockets.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
