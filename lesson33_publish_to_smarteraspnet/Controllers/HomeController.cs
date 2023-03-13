4b199106e6e82ad2fd90eae1821ce5659ec65d7b
gornvan <name@example.com>
2023-03-13 20:33:12 +0100

    lesson 33 - publishing to smarteraspnet

---
+++ lesson33_publish_to_smarteraspnet/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using lesson33_publish_to_smarteraspnet.Models;
+
+namespace lesson33_publish_to_smarteraspnet.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ lesson33_publish_to_smarteraspnet/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace lesson33_publish_to_smarteraspnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
