05acaf71921ff2637ac28d51c3a24b3328d80fca
gornvan <name@example.com>
2023-03-15 18:02:57 +0100

    create basic gRPC client project for the Server, by adding the GRPC nugets to Console project and linking Protobuf in its .csproj to the .proto file inside Server

---
+++ lesson34_gRPC/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GreeterGrpcService;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7098");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey(); ;
+
+
