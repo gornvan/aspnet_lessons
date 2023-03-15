b5750907216080b7bbe391747fc2f35449b30146
gornvan <name@example.com>
2023-03-15 19:47:11 +0100

    Add WebAPI services to the previously GRPC-only Application

--- lesson34_gRPC/Server/GreeterGrpcService/Program.cs
+++ lesson34_gRPC/Server/GreeterGrpcService/Program.cs
@@ -12,15 +12,31 @@ namespace GreeterGrpcService
             // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
             // Add services to the container.
-            builder.Services.AddGrpc();
+
+            builder.Services.AddControllers(); // for webapi/mvc controllers
+
+            builder.Services.AddGrpc(); // GRPC
+
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            app.MapGrpcService<GreeterService>();
-            app.MapGet("/", 
-                () => "Communication with gRPC endpoints must be made through a gRPC client." +
-                " To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+            // UseRouting is required if we specify endpoints via app.UseEndpoints (...
+            //app.UseRouting();
+
+            app.MapControllers().RequireHost("*:5139");
+
+            //Configure the HTTP request pipeline.
+            app.MapGrpcService<GreeterService>().RequireHost("*:7098");
+
+            app.MapGet("/",
+                () =>
+                {
+                    return "Communication with gRPC endpoints is on port 7098," +
+                    " and it must be made through a gRPC client." +
+                    " To learn how to create a client," +
+                    "visit: https://go.microsoft.com/fwlink/?linkid=2086909";
+                })
+                .RequireHost("*:5139");
 
             app.Run();
         }
