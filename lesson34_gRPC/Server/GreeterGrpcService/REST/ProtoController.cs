5f30581765c2c6412e95a5ca977557078ef62f33
gornvan <name@example.com>
2023-03-15 19:48:05 +0100

    Add a convinience WebApi endpoint for returning .proto file from /api/Proto

---
+++ lesson34_gRPC/Server/GreeterGrpcService/REST/ProtoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+namespace GreeterGrpcService.REST
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProtoController : ControllerBase
+    {
+        private const string ProtoFilePath = "./Protos/greet.proto";
+
+        [HttpGet]
+        public FileResult GetProtobufDefinitionsFile()
+        {
+            var steam = new FileStream(ProtoFilePath, FileMode.Open);
+            return File(steam, "text/plain", fileDownloadName: "greeter.proto");
+        }
+    }
+}
